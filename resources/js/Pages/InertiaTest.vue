ce362423b49d0554a2b17a33ba0d93f2cfe402b2
y_shirahama <yuuya-shirahama@baby-calendar.jp>
2023-03-02 20:45:55 +0900

    InertiaTestページの追加

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template xmlns="http://www.w3.org/1999/html">
+    <p>Inertiaテストです。</p>
+    <a href="/">aタグ経由です。</a><br/>
+    <Link href="/">Link経由です。</Link>
+</template>
+
+<style scoped>
+
+</style>
